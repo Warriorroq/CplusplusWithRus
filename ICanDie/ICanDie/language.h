4f9b5d93fabd8654b063aaf8094e5cd683a52735
Warriorroq <Warriorroq@gmail.com>
2021-01-05 21:46:35 +0200

    Создание лабиринта

---
+++ ICanDie/ICanDie/language.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#define приглуши_хуйню_нахуй using namespace std;
+#define слышь cout <<
+#define еще <<
+#define попизди cin >> 
+#define продолжай >>
+#define заткнись <<endl;
+
+#define не_трогать const 
+#define бл€ ; 
+#define это =
+#define пиздни_циферку rand()
+#define символ char
+#define ниху€ void 
+#define число int 
+#define дробь double
+#define факт bool
+#define хуйн€ false
+#define пиздато true
+
+#define верни return
+#define если if(
+#define скобка (
+#define то )
+#define пошЄл {
+#define нахуй }
+#define иначе else 
+
+#define пиздит !=
+#define сравнить ==
+#define больше >
+#define больше_равно >=
+#define меньше_равно <=
+#define меньше <
+
+#define и &&
+#define или ||
+
+#define пока while(
+#define пройдись for(
+#define когда_рак_свистнет ;;
+
+#define ноль 0
+#define увеличь ++
+#define уменшь --
+#define один 1
+#define стенка static_cast<char>(176)
+#define стенку static_cast<char>(176)
