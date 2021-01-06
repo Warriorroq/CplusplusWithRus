053d21e3149db44c182d815d3a95782c394a9f5d
Warriorroq <Warriorroq@gmail.com>
2021-01-06 13:08:21 +0200

    перенесение комментов

--- ICanDie/ICanDie/ICanDie.cpp
+++ ICanDie/ICanDie/ICanDie.cpp
@@ -87,3 +87,53 @@
 		нахуй
 	верни ноль бля
 нахуй
+
+/*
+#define приглуши_хуйню_нахуй using namespace std;
+#define слышь cout <<
+#define еще <<
+#define попизди cin >>
+#define продолжай >>
+#define заткнись <<endl;
+
+#define не_трогать const
+#define бля ;
+#define это =
+#define пиздни_циферку rand()
+#define символ char
+#define нихуя void
+#define число int
+#define дробь double
+#define факт bool
+#define хуйня false
+#define пиздато true
+
+#define верни return
+#define если if(
+#define скобка (
+#define то )
+#define пошёл {
+#define нахуй }
+#define иначе else
+
+#define пиздит !=
+#define сравнить ==
+#define больше >
+#define больше_равно >=
+#define меньше_равно <=
+#define меньше <
+
+#define и &&
+#define или ||
+
+#define пока while(
+#define пройдись for(
+#define когда_рак_свистнет ;;
+
+#define ноль 0
+#define увеличь ++
+#define уменшь --
+#define один 1
+#define стенка static_cast<char>(176)
+#define стенку static_cast<char>(176)
+*/
